f24766e67bd0ba58243e81376877e7c6bd10f995
Manuel Rabade <manuel@rabade.net>
2015-02-23 14:07:01 -0600

    monitoreo calidad de la señal

--- firmware/SismoAlerta/SismoAlerta.h
+++ firmware/SismoAlerta/SismoAlerta.h
@@ -1,14 +1,14 @@
 /* entrada/salida
    --------------
 
-   SI4707_RESET: pin reset de si4707
-   USER_BUTTON: pin del botón de usuario
-   BUZZER: pin del zumbador
-   EXT_POWER: pin analógico para medir voltaje externo
-   POWER_LED_RED: pin del led rojo de energía
-   POWER_LED_GREEN: pin del led verde de energía
-   SIGNAL_LED_RED: pin del led rojo de señal 
-   SIGNAL_LED_GREEN: pin del led verde de señal */
+   SI4707_RESET: pin reset si4707
+   USER_BUTTON: pin botón usuario
+   BUZZER: pin zumbador
+   EXT_POWER: pin analógico voltaje externo
+   POWER_LED_RED: pin led rojo energía
+   POWER_LED_GREEN: pin led verde energía
+   SIGNAL_LED_RED: pin led rojo señal
+   SIGNAL_LED_GREEN: pin led verde señal */
 
 #define SI4707_RESET 10
 #define USER_BUTTON 5
@@ -23,30 +23,34 @@
    -----------------------
 
    SCAN_TIMES: numero de escaneos
-   SCAN_DELAY: retardo entre escaneo de canales (milisegundos)
-   SCAN_SAMPLES: muestras por canal
-   SCAN_SAMPLE_DELAY: retardo para tomar muestra (milisegundos)
-   SCAN_SAMPLE_LOG: informar de cada muestra del escaneo (0 o 1)
-   TUNE_MIN_SNR: relación señal/ruido mínima (db)
+   SCAN_DELAY: retardo entre escaneos (milisegundos)
+   SCAN_SAMPLES: muestras por canal por escaneo
+   SCAN_SAMPLE_DELAY: retardo para muestrear canal (milisegundos)
+   SCAN_VERBOSE: informar cada muestra del escaneo (0 o 1)
    TUNE_MIN_RSSI: potencia mínima (db)
+   TUNE_MIN_SNR: relación señal/ruido mínima (db)
    TUNE_LOST_DELAY: retardo para declarar un canal perdido (milisegundos)
-   ALARM_TIME: duración de la alarma (milisegundos)
-   TEST_TIME: duración de la prueba de alarma (milisegundos)
+   ALARM_TIME: duración alarma (milisegundos)
+   TEST_TIME: duración prueba de alarma (milisegundos)
    SERVICE_USER_FREQ: frecuencia de actualización al usuario (hertz)
    USER_BUTTON_DEBOUNCE: retardo antirebote del botón de usuario (milisegundos)
-   USER_BUTTON_TEST_DELAY: retardo para ejecutar prueba (milisegundos)
+   USER_BUTTON_TEST_DELAY: retardo para ejecutar prueba de usuario (milisegundos)
    CHARGE_VOLTAGE: voltaje mínimo para cargar la batería interna (milivolts)
-   EXT_POWER_K_REL: constante de confiabilidad de la muestra de voltaje externo (0 a 1)
-   UPDATE_DELAY: retardo para actualizar al usuario (milisegundos) 
-   SELFTEST_DELAY: retardo autoprueba (milisegundos) */
+   EXT_POWER_A: constante para el promedio móvil del voltaje externo (0 a 1)
+   UPDATE_DELAY: retardo para actualizar al usuario (milisegundos)
+   SELFTEST_DELAY: retardo autoprueba (milisegundos)
+   SNR_RSSI_LOG: monitoreo calidad de señal (0 o 1)
+   SNR_RSSI_LOG_SAMPLES: muestras de calidad de señal a promediar
+   SNR_RSSI_SAMPLE_DELAY: retardo para muestrear la calidad de señal (milisegundos)
+   SNR_RSSI_VERBOSE: informar cada muestra de calidad de señal (0 o 1) */
 
 #define SCAN_TIMES 3
 #define SCAN_DELAY 1000
 #define SCAN_SAMPLES 3
 #define SCAN_SAMPLE_DELAY 300
-#define SCAN_SAMPLE_LOG 0
-#define TUNE_MIN_SNR 2
-#define TUNE_MIN_RSSI 0
+#define SCAN_VERBOSE 0
+#define TUNE_MIN_RSSI 2
+#define TUNE_MIN_SNR 5
 #define TUNE_LOST_DELAY 300000 // 5 minutos
 #define ALARM_TIME 60000 // 1 minuto
 #define TEST_TIME 10000 // 10 segundos
@@ -54,9 +58,13 @@
 #define USER_BUTTON_DEBOUNCE 100
 #define USER_BUTTON_TEST_DELAY 3000 // 3 segundos
 #define CHARGE_VOLTAGE 3750
-#define EXT_POWER_K_REL 0.60
+#define EXT_POWER_A 0.70
 #define UPDATE_DELAY 500
 #define SELFTEST_DELAY 1000
+#define SNR_RSSI_LOG 1
+#define SNR_RSSI_LOG_SAMPLES 240 // 20 minutos
+#define SNR_RSSI_SAMPLE_DELAY 5000 // 5 segundos
+#define SNR_RSSI_VERBOSE 0
 
 /* weather band radio
    ------------------
@@ -70,28 +78,30 @@
 /* monitoreo
    ---------
 
-   YUN_LOGGER: activar modo arduino yún con logger (0 o 1)
-   YUN_TWITTER: activar modo arduino yún con retransmisor twitter (0 o 1) */
+   YUN_LOGGER: modo arduino yún con bitácora (0 o 1)
+   YUN_TWITTER: modo arduino yún con retransmisor twitter (0 o 1) */
 
 #define YUN_LOGGER 0
 #define YUN_TWITTER 0
 
-/* constantes
-   ---------- */
+/* consola
+   ------- */
 
 #if (YUN_LOGGER || YUN_TWITTER)
-#define YUN_MONITOR 1
 #define LOG Console
 #else
 #define LOG Serial
 #endif
-#define SCAN_AVG_K (SCAN_TIMES * SCAN_SAMPLES)
+
+/* constantes
+   ---------- */
+
 #define SAME_EOM_DET 0
 #define SAME_PRE_DET 1
 #define SAME_HDR_DET 2
 #define SAME_HDR_RDY 3
 #define SAME_TIMEOUT 6000
 #define SAME_TEST_TIMEOUT 11400000 // 3 horas y 10 minutos
-#define SERVICE_USER_MICROSEC 1000000 / SERVICE_USER_FREQ
+#define SERVICE_USER_MICROSEC (1000000 / SERVICE_USER_FREQ)
 #define USER_BUTTON_INT_MAX ((USER_BUTTON_DEBOUNCE * SERVICE_USER_FREQ) / 1000)
-#define EXT_POWER_K_MV (3300 / 512)
+#define EXT_POWER_M (3300 / 512)
--- firmware/SismoAlerta/SismoAlerta.ino
+++ firmware/SismoAlerta/SismoAlerta.ino
@@ -5,12 +5,12 @@
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -19,69 +19,74 @@
 #include <TimerOne.h>
 #include <Si4707.h>
 #include "SismoAlerta.h"
-
-#ifdef YUN_MONITOR
+#if (YUN_LOGGER || YUN_TWITTER)
 #include <Bridge.h>
 #include <Console.h>
-#endif
-
 #if YUN_LOGGER
 #include <Process.h>
 #endif
-
 #if YUN_TWITTER
 #include <HttpClient.h>
 #include "yun_twitter.h"
 #endif
+#endif
 
-/* variables y objetos globales 
+/* variables y objetos globales
    ============================ */
 
 // weather band radio
 Si4707 wbr(SI4707_RESET);
 const unsigned long wbr_channels[] = { WBR_CHANNELS };
 
-// banderas y parámetros
-boolean scan_error_flag = 0;
-byte tune_channel = 0;
-boolean asq_prev_status = 0;
-byte same_prev_state = 0;
-byte same_headers_count = 0;
+// maquina de estados
 enum fsm_states { SCAN, LISTEN } state = SCAN;
 
-// timers
-unsigned long tune_timer = 0;
-unsigned long same_timer = 0;
-unsigned long same_test_timer = 0;
-volatile unsigned long user_test_timer = 0;
-volatile unsigned long same_alert_timer = 0;
+// escaneo
+boolean scan_error_flag = 0;
+byte tune_channel;
+
+// monitoreo
+boolean asq_prev_status;
+byte same_prev_state, same_headers_count;
+unsigned long tune_timer, same_timer, same_test_timer;
 
 // servicio al usuario
+volatile unsigned long user_test_timer = 0;
+volatile unsigned long same_alert_timer = 0;
 volatile unsigned int user_button_integrator = 0;
 volatile boolean user_button_prev_state = HIGH;
 volatile unsigned long last_user_button_push = 0;
-volatile unsigned int last_ext_power_sample = 0;
+volatile unsigned int ext_power_analog_avg;
 volatile unsigned long last_update = 0;
 volatile boolean update_state = LOW;
 
-// versión monitor
+// monitoreo calidad de señal
+#if SNR_RSSI_LOG
+float snr_avg, rssi_avg;
+unsigned long snr_rssi_sample_timer;
+int snr_rssi_samples_count;
+#endif
+
+// modo arduino yún con bitácora
 #if YUN_LOGGER
 Process proc;
 #endif
+
+// modo arduino yún con retransmisor twitter
 #if YUN_TWITTER
 HttpClient http;
 #endif
 
-/* configuración 
+/* configuración
    ============= */
 
 void setup() {
-  // configuración consola telnet o puerto serial
-#ifdef YUN_MONITOR
+  // consola telnet o puerto serial
+#if (YUN_LOGGER || YUN_TWITTER)
   Bridge.begin();
   LOG.begin();
 #if YUN_LOGGER
-  // proceso logger (telnet localhost 6571 | ...)
+  // proceso logger (nc localhost 6571 | ...)
   proc.runShellCommandAsynchronously("/root/sismo_alerta_logger");
   while (!LOG);
 #endif
@@ -90,7 +95,7 @@ void setup() {
   LOG.println();
 #endif
 
-  // configuramos pines de entrada/salida
+  // entrada/salida
   LOG.println(F("SETUP"));
   pinMode(BUZZER, OUTPUT);
   pinMode(POWER_LED_RED, OUTPUT);
@@ -98,35 +103,35 @@ void setup() {
   pinMode(SIGNAL_LED_RED, OUTPUT);
   pinMode(SIGNAL_LED_GREEN, OUTPUT);
   pinMode(USER_BUTTON, INPUT_PULLUP);
-  
+
   // autoprueba
   LOG.println(F("SELFTEST"));
   digitalWrite(POWER_LED_GREEN, HIGH);
   digitalWrite(SIGNAL_LED_GREEN, HIGH);
   digitalWrite(POWER_LED_RED, LOW);
   digitalWrite(SIGNAL_LED_RED, LOW);
-  digitalWrite(BUZZER, HIGH);  
+  digitalWrite(BUZZER, HIGH);
   delay(SELFTEST_DELAY);
   digitalWrite(POWER_LED_GREEN, LOW);
   digitalWrite(SIGNAL_LED_GREEN, LOW);
   digitalWrite(POWER_LED_RED, HIGH);
   digitalWrite(SIGNAL_LED_RED, HIGH);
-  digitalWrite(BUZZER, LOW);  
+  digitalWrite(BUZZER, LOW);
   delay(SELFTEST_DELAY);
-  
-  // inicializamos si4707
+
+  // si4707
   LOG.println(F("BEGIN"));
   if (wbr.begin()) {
-    // iniciamos la interrupción de servicio al usuario
-    last_ext_power_sample = analogRead(EXT_POWER);
+    // interrupción de servicio al usuario
+    ext_power_analog_avg = analogRead(EXT_POWER);
     Timer1.initialize(SERVICE_USER_MICROSEC);
     Timer1.attachInterrupt(service_user);
-    // configuramos si4707
+    // configuración si4707
     wbr.setMuteVolume(1);
     wbr.setSNR(TUNE_MIN_SNR);
     wbr.setRSSI(TUNE_MIN_RSSI);
   } else {
-    // error al inicializar si4707
+    // error si4707
     digitalWrite(POWER_LED_RED, HIGH);
     digitalWrite(POWER_LED_GREEN, LOW);
     digitalWrite(SIGNAL_LED_RED, LOW);
@@ -136,7 +141,7 @@ void setup() {
   }
 }
 
-/* maquina de estados 
+/* maquina de estados
    ================== */
 
 void loop() {
@@ -150,31 +155,34 @@ void loop() {
   }
 }
 
-/* escaneo 
+/* escaneo
    ======= */
 
 void scan() {
   LOG.println(F("SCAN_START"));
 
-  // tomamos muestras del rssi y snr en todos los canales
-  unsigned int rssi_sum[WBR_CHANNELS_SIZE] = { 0 };
-  unsigned int snr_sum[WBR_CHANNELS_SIZE] = { 0 };
+  // promedios de rssi y snr
+  float scan_rssi_avg[WBR_CHANNELS_SIZE] = { 0 };
+  float scan_snr_avg[WBR_CHANNELS_SIZE] = { 0 };
+
+  // escaneo
   for (byte t = 0; t < SCAN_TIMES; t++) {
     for (byte c = 0; c < WBR_CHANNELS_SIZE; c++) {
       wbr.setWBFrequency(wbr_channels[c]);
       for (byte s = 0; s < SCAN_SAMPLES; s++) {
         delay(SCAN_SAMPLE_DELAY);
+        // muestreo
         unsigned int rssi = wbr.getRSSI();
-        rssi_sum[c] += rssi;
         unsigned int snr = wbr.getSNR();
-        snr_sum[c] += snr;
-#if SCAN_SAMPLE_LOG
+        // promedio móvil acumulativo
+        scan_rssi_avg[c] += (rssi - scan_rssi_avg[c]) / (3 * t + s + 1);
+        scan_snr_avg[c] += (snr - scan_snr_avg[c]) / (3 * t + s + 1);
+        // reporte muestra
+#if SCAN_VERBOSE
         LOG.print(F("SCAN_SAMPLE,"));
-        LOG.print(t, DEC);
-        LOG.print(F(","));
-        LOG.print(s, DEC);
-        LOG.print(F(","));
         LOG.print(wbr_channels[c], DEC);
+        LOG.print(F(","));
+        LOG.print(3 * t + s, DEC);
         LOG.print(F(","));
         LOG.print(rssi, DEC);
         LOG.print(F(","));
@@ -185,52 +193,81 @@ void scan() {
     delay(SCAN_DELAY);
   }
 
-  // calculamos rssi/snr promedio y encontramos el mejor canal
+  // mejor canal
   byte best_channel = 0;
-  unsigned int best_rssi = 0;
-  unsigned int best_snr = 0;
+  float best_rssi = 0;
+  float best_snr = 0;
+
+  // reporte promedios y búsqueda del mejor canal
   for (byte c = 0; c < WBR_CHANNELS_SIZE; c++) {
-    float rssi_avg = rssi_sum[c] / (float) SCAN_AVG_K;
-    float snr_avg = snr_sum[c] / (float) SCAN_AVG_K;
-    if (rssi_avg >= best_rssi && snr_avg >= best_snr) {
-      best_channel = c;
-      best_rssi = rssi_avg;
-      best_snr = snr_avg;
-    }
-    LOG.print(F("SCAN,"));
+    LOG.print(F("SCAN_AVG,"));
     LOG.print(wbr_channels[c], DEC);
     LOG.print(F(","));
-    LOG.print(rssi_avg, 2);
+    LOG.print(SCAN_TIMES * SCAN_SAMPLES, DEC);
     LOG.print(F(","));
-    LOG.println(snr_avg, 2);
+    LOG.print(scan_rssi_avg[c], 2);
+    LOG.print(F(","));
+    LOG.println(scan_snr_avg[c], 2);
+    if (scan_rssi_avg[c] >= best_rssi && scan_snr_avg[c] >= best_snr) {
+      best_channel = c;
+      best_rssi = scan_rssi_avg[c];
+      best_snr = scan_snr_avg[c];
+    }
   }
 
-  if (best_rssi >= TUNE_MIN_RSSI && best_snr >= TUNE_MIN_SNR) {
-    // si encontramos un canal lo sintonizamos y pasamos al estado monitoreo
+  // ¿se encontró un canal valido?
+  if (best_rssi >= (float) TUNE_MIN_RSSI && best_snr >= (float) TUNE_MIN_SNR) {
+    // sintonizar canal y pasar a estado monitoreo
     state = LISTEN;
     scan_error_flag = 0;
     tune_channel = best_channel;
     wbr.setWBFrequency(wbr_channels[tune_channel]);
     LOG.print(F("SCAN_OK,"));
     LOG.println(wbr_channels[best_channel], DEC);
+    // reinicio monitoreo
+    asq_prev_status = 0;
+    same_prev_state = 0;
+    same_headers_count = 0;
+    tune_timer = millis();
+    same_timer = 0;
+    same_test_timer = 0;
+    // reinicio monitoreo calidad de señal
+#if SNR_RSSI_LOG
+    snr_rssi_sample_timer = millis();
+    rssi_avg = 0;
+    snr_avg = 0;
+    snr_rssi_samples_count = 0;
+#endif
   } else {
-    // repetimos el escaneo
-    scan_error_flag = 1;  // bandera para encender el red de señal en rojo
+    // repetir escaneo
+    scan_error_flag = 1;
     LOG.println(F("SCAN_ERROR"));
     delay(SCAN_DELAY);
   }
 }
 
-/* monitoreo 
+/* monitoreo
    ========= */
 
 void listen() {
-  // monitoreamos si el canal aun es valido
+  // ¿el canal es valido?
   if (wbr.getRSQ()) {
     tune_timer = millis();
   } else {
     if (millis() - tune_timer > TUNE_LOST_DELAY) {
-      state = SCAN;      
+      // reporte calidad de señal
+#if SNR_RSSI_LOG
+      LOG.print(F("SNR_RSSI_AVG,"));
+      LOG.print(wbr_channels[tune_channel], DEC);
+      LOG.print(F(","));
+      LOG.print(snr_rssi_samples_count, DEC);
+      LOG.print(F(","));
+      LOG.print(rssi_avg, 2);
+      LOG.print(F(","));
+      LOG.println(snr_avg, 2);
+#endif
+      // pasar a estado escaneo
+      state = SCAN;
       same_test_timer = 0;
       LOG.print(F("TUNE_LOST,"));
       LOG.println(wbr_channels[tune_channel], DEC);
@@ -238,7 +275,7 @@ void listen() {
     }
   }
 
-  // monitoreo del tono 1050 khz
+  // monitoreo tono 1050 khz
   boolean asq_status = wbr.getASQ();
   if (asq_prev_status != asq_status) {
     if (asq_status) {
@@ -258,8 +295,8 @@ void listen() {
     case SAME_EOM_DET:
       // fin del mensaje
       LOG.println(F("SAME_EOM_DET"));
+      // ¿se recibieron una o dos cabeceras?
       if (same_headers_count > 0) {
-        // si recibimos solo 1 o 2 cabeceras
         same_message();
       } else {
         same_reset();
@@ -283,7 +320,7 @@ void listen() {
       LOG.println(same_headers_count);
       break;
     }
-    // procesamos mensaje después de recibir tres cabeceras
+    // ¿se recibieron tres cabeceras?
     if (same_headers_count == 3) {
       same_message();
     }
@@ -294,8 +331,8 @@ void listen() {
   // timeout mensaje same
   if (same_timer && millis() - same_timer > SAME_TIMEOUT) {
     LOG.println(F("SAME_TIMEOUT"));
+    // ¿se recibieron una o dos cabeceras?
     if (same_headers_count > 0) {
-      // si recibimos solo 1 o 2 cabeceras
       same_message();
     } else {
       same_reset();
@@ -307,23 +344,77 @@ void listen() {
     same_test_timer = 0;
     LOG.println(F("SAME_TEST_TIMEOUT"));
   }
+
+  // monitoreo calidad de señal
+#if SNR_RSSI_LOG
+  if (millis() - snr_rssi_sample_timer > SNR_RSSI_SAMPLE_DELAY) {
+    // muestreo
+    unsigned int rssi = wbr.getRSSI();
+    unsigned int snr = wbr.getSNR();
+    // promedio móvil acumulativo
+    rssi_avg += (rssi - rssi_avg) / (snr_rssi_samples_count + 1);
+    snr_avg += (snr - snr_avg) / (snr_rssi_samples_count + 1);
+    // reporte muestra
+#if SNR_RSSI_VERBOSE
+    LOG.print(F("SNR_RSSI_SAMPLE,"));
+    LOG.print(wbr_channels[tune_channel], DEC);
+    LOG.print(F(","));
+    LOG.print(snr_rssi_samples_count, DEC);
+    LOG.print(F(","));
+    LOG.print(rssi, DEC);
+    LOG.print(F(","));
+    LOG.println(snr, DEC);
+#endif
+    // actualización timer y conteo
+    snr_rssi_sample_timer = millis();
+    snr_rssi_samples_count++;
+  }
+  if (snr_rssi_samples_count == SNR_RSSI_LOG_SAMPLES) {
+    // reporte calidad de señal
+    LOG.print(F("SNR_RSSI_AVG,"));
+    LOG.print(wbr_channels[tune_channel], DEC);
+    LOG.print(F(","));
+    LOG.print(snr_rssi_samples_count, DEC);
+    LOG.print(F(","));
+    LOG.print(rssi_avg, 2);
+    LOG.print(F(","));
+    LOG.println(snr_avg, 2);
+    // reinicio promedios y contador
+    rssi_avg = 0;
+    snr_avg = 0;
+    snr_rssi_samples_count = 0;
+  }
+#endif
 }
 
-/* mensajes same 
+/* mensajes same
    ============= */
 
 void same_message() {
-  // obtenemos mensaje
   byte size = wbr.getSAMESize();
+  // ¿mensaje vacío?
   if (size < 1) {
     LOG.println(F("SAME_EMPTY"));
     return;
   }
+
+  // adquisición mensaje
   byte msg[size];
   wbr.getSAMEMessage(size, msg);
   same_reset();
-  
-  // interpretamos evento
+
+  // reportamos mensaje
+  LOG.print(F("SAME,"));
+  for (byte i = 0; i < size; i++) {
+    if (msg[i] > 31 && msg[i] < 127) {
+      LOG.write(msg[i]);
+    } else {
+      LOG.print(F("."));
+    }
+  }
+  LOG.println();
+
+  // interpretación mensaje
   if (size >= 38) {
     if (msg[5] == 'E' &&
         msg[6] == 'Q' &&
@@ -339,44 +430,31 @@ void same_message() {
       LOG.println(F("SAME_RWT"));
     }
   }
-  
-  // reportamos mensaje
-  LOG.print(F("SAME,"));
-  for (byte i = 0; i < size; i++) {
-    if (msg[i] > 31 && msg[i] < 127) {
-      LOG.write(msg[i]);
-    } else {
-      LOG.print(".");
-    }
-  }
-  LOG.println();
 
+  // publicación mensaje
 #if YUN_TWITTER
-  // publicamos mensaje
   String url = WS_URL;
   url += "?secret=";
   url += WS_SECRET;
   url += "&same=";
-
   for (byte i = 0; i < size; i++) {
     url += (int) msg[i];
     url += ':';
   }
-
   http.getAsynchronously(url);
-  LOG.print("URL,");
+  LOG.print(F("URL,"));
   LOG.println(url);
 #endif
 }
 
 void same_reset() {
-  // limpiamos buffer, timer y contador de cabeceras
+  // reinicio buffer, timer y contador
   wbr.clearSAMEBuffer();
   same_timer = 0;
-  same_headers_count = 0;  
+  same_headers_count = 0;
 }
 
-/* servicio al usuario 
+/* servicio al usuario
    =================== */
 
 void service_user() {
@@ -398,7 +476,7 @@ void service_user() {
     user_button_integrator = USER_BUTTON_INT_MAX;
   }
 
-  // procesamos botón de usuario
+  // monitoreo botón de usuario
   if (user_button_prev_state != user_button_state) {
     if (user_button_state == LOW) {
       user_test_timer = 0;
@@ -414,12 +492,11 @@ void service_user() {
     }
   }
 
-  // filtro voltaje externo
-  unsigned int ext_power_sample = (float) EXT_POWER_K_REL * analogRead(EXT_POWER);
-  ext_power_sample += (1 - (float) EXT_POWER_K_REL) * last_ext_power_sample;
-  last_ext_power_sample = ext_power_sample;
-  
-  // actualizamos
+  // promedio móvil compensado del voltaje externo
+  ext_power_analog_avg *= 1 - (float) EXT_POWER_A;
+  ext_power_analog_avg += analogRead(EXT_POWER) * (float) EXT_POWER_A;
+
+  // actualización
   if (millis() - last_update > UPDATE_DELAY) {
     update_state = !update_state;
     if (same_alert_timer || user_test_timer) {
@@ -432,14 +509,17 @@ void service_user() {
 }
 
 void alert_user() {
+  // alerta usuario
   digitalWrite(SIGNAL_LED_RED, update_state);
   digitalWrite(SIGNAL_LED_GREEN, LOW);
   digitalWrite(POWER_LED_RED, !update_state);
   digitalWrite(POWER_LED_GREEN, LOW);
   digitalWrite(BUZZER, update_state);
+  // alerta sísmica
   if (millis() - same_alert_timer > ALARM_TIME) {
     same_alert_timer = 0;
   }
+  // prueba de usuario
   if (millis() - user_test_timer > TEST_TIME) {
     user_test_timer = 0;
   }
@@ -448,39 +528,39 @@ void alert_user() {
 void update_user() {
   // buzzer apagado
   digitalWrite(BUZZER, LOW);
-  
+
   // led señal
   if (state == SCAN) {
-    // escaneando canales
+    // escaneo
     if (scan_error_flag) {
-      // error en escaneo
-      digitalWrite(SIGNAL_LED_GREEN, LOW);    
+      // error
+      digitalWrite(SIGNAL_LED_GREEN, LOW);
       digitalWrite(SIGNAL_LED_RED, HIGH);
     } else {
       // sintonizando por primera vez
       digitalWrite(SIGNAL_LED_GREEN, LOW);
-      digitalWrite(SIGNAL_LED_RED, LOW);          
+      digitalWrite(SIGNAL_LED_RED, LOW);
     }
   } else {
-    // con canal sintonizado
-    digitalWrite(SIGNAL_LED_RED, LOW);      
+    // monitoreo
+    digitalWrite(SIGNAL_LED_RED, LOW);
     if (same_test_timer) {
-      // con prueba same vigente
+      // prueba same vigente
       digitalWrite(SIGNAL_LED_GREEN, HIGH);
     } else {
-      // sin prueba same vigente
+      // no hay prueba same vigente
       digitalWrite(SIGNAL_LED_GREEN, update_state);
     }
   }
-  
+
   // led energía
-  volatile unsigned int milivolts = last_ext_power_sample * (float) EXT_POWER_K_MV;
+  volatile unsigned int milivolts = ext_power_analog_avg * (float) EXT_POWER_M;
   if (milivolts >= CHARGE_VOLTAGE) {
-    // con energía externa
+    // energía externa
     digitalWrite(POWER_LED_RED, LOW);
     digitalWrite(POWER_LED_GREEN, HIGH);
   } else {
-    // sin energía externa
+    // batería
     digitalWrite(POWER_LED_RED, LOW);
     digitalWrite(POWER_LED_GREEN, !update_state);
   }
